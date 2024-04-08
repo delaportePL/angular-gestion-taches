bff932375b4150c1b8ead0640685139e6d6fcc00
delaportePL <pierre-louis.delaporte@hotmail.fr>
2024-04-08 19:29:21 +0200

    Corrige le routing

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
 import { TaskListOverlayComponent } from './features/task-list-overlay/task-list-overlay.component';
-import { AppComponent } from './app.component';
+import { TaskDetailsComponent } from './features/task-details/task-details.component';
 
 const routes: Routes = [
-  // { path: '', component: AppComponent},
-  { path: 'liste-taches', component: TaskListOverlayComponent },
-  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+  { 
+    path: '', 
+    component: TaskListOverlayComponent,
+    children: [{ path:'task/:id', component: TaskDetailsComponent}]
+  },
+  { path: '**', component: PageNotFoundComponent,  pathMatch: 'full' },  // Route for a 404 page
 ];
 
 @NgModule({
